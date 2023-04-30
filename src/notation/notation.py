3fd140aff96c5063c7c6c9de692ad3d18c44f556
Adam Patterson <adam@faranalytics.net>
2023-04-30 15:57:38 -0400

    Add __contains__.

--- src/notation/notation.py
+++ src/notation/notation.py
@@ -3,10 +3,10 @@ class Notation:
     def __init__(self, annotation={}):
         self.__dict__ = annotation
 
-    def __getitem__(self, name):
-        return self.__dict__[name]
+    def __getitem__(self, key):
+        return self.__dict__[key]
 
-    def __setitem__(self, name, value):
+    def __setitem__(self, key, value):
         self.__dict__[name] = value
 
     def __str__(self):
@@ -15,8 +15,13 @@ class Notation:
     def __repr__(self):
         return f'{self.__class__.__name__}(annotation={self.__dict__})'
 
+    def __contains__(self, key):
+        return key in self.__dict__
+
     def __call__(self, *args, **kwargs):
         def decorator(fn):
+            print('function argument', fn)
             self.__dict__[fn] = {**{index:arg for index, arg in enumerate(args)}, **kwargs}
             return fn
+        print('decorator function', decorator)
         return decorator
